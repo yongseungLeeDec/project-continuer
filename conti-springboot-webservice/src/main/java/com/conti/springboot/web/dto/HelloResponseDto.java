05bc2655f7dc405b3e6a3613abfdc0437a62fdc5
yongseungLeeDec <yonson.dev@gmail.com>
2022-06-17 16:41:21 +0900

    impl: implemented controller & dto for practice

---
+++ conti-springboot-webservice/src/main/java/com/conti/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,24 @@
+package com.conti.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+
+//    public HelloResponseDto(String name, int amount) {
+//        this.name = name;
+//        this.amount = amount;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public int getAmount() {
+//        return amount;
+//    }
+}
---
+++ conti-springboot-webservice/src/test/java/com/conti/springboot/web/HelloControllerTest.java
@@ -0,0 +1,37 @@
+package com.conti.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.hamcrest.Matchers.is;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void returns_hello() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(MockMvcRequestBuilders.get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
+    }
+
+    @Test
+    public void returns_helloDto() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(MockMvcRequestBuilders.get("/hello/dto").param("name", name).param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk()).andExpect(jsonPath("$.name", is(name))).andExpect(jsonPath("$.amount", is(amount)));
+    }
+}
