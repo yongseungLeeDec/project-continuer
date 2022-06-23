a51592db8c3d5a1063061d781caae35d7e64bcc9
yongseungLeeDec <yonson.dev@gmail.com>
2022-06-23 21:21:01 +0900

    file: implemented PostsListResponseDto class

---
+++ conti-springboot-webservice/src/main/java/com/conti/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,21 @@
+package com.conti.springboot.web.dto;
+
+import com.conti.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
